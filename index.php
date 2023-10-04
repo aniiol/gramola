7affa9f52267b09f1bd48e045a54e62278b46a43
aniolmorenobatlle <amoreno@cendrassos.net>
2023-10-04 12:22:24 +0200

    Escriure la canço mes reproduida

--- index.php
+++ index.php
@@ -12,16 +12,45 @@
         }
     }
 
-    setcookie("playlist_id", $playlistId, strtotime("+7 days")); // guarda la cookie per 7 dies
+    setcookie("playlist_id", $playlistId, strtotime("+1 year")); // guarda la cookie per 1 any
 
     // Mirem que $files no estigui buit i que la ruta esta ben apuntada (si no es fa aixo, salta un error quan vull eliminar alguna playlist)
     if (isset($files[$playlistId]) && file_exists($files[$playlistId])) {
         $data = file_get_contents($files[$playlistId]);
         $playlist = json_decode($data, true);
+        
+       // Verifiquem si la cookie existeix i la descodifiquem amb decode
+        if (isset($_COOKIE["playlist_reproducciones"])) {
+            $playlistReproducciones = json_decode($_COOKIE["playlist_reproducciones"], true);
+        } else {
+            $playlistReproducciones = array();
+        }
+
+        // Incrementa el contador de reproducciones de la playlist actual
+        if (isset($playlistReproducciones[$playlistId])) {
+            $playlistReproducciones[$playlistId]++;
+        } else {
+            $playlistReproducciones[$playlistId] = 1;
+        }
+
+        setcookie("playlist_reproducciones", json_encode($playlistReproducciones), strtotime("+1 year"));
+    }
+
+    if (isset($_COOKIE["playlist_reproducciones"])) {
+        $playlistReproducciones = json_decode($_COOKIE["playlist_reproducciones"], true);
+    } else {
+        $playlistReproducciones = array();
     }
     
+    // Trobar l'index de la mes reproduiad
+    $playlistMesReproduida = array_search(max($playlistReproducciones), $playlistReproducciones);
+
+    // Assignar el nom de la canço mes reproduida
+    $nomMesReproduida = playlistName($files[$playlistMesReproduida]);
+
+    
     // Funcio per saber el titol dels arxius json
-    function playlistName ($file) {
+    function playlistName($file) {
         $data = file_get_contents($file);
         $playlist = json_decode($data, true);
 
@@ -29,13 +58,12 @@
     }
 
     // Funcio per saber la descripcio dels arxius json
-    function playlistDescription ($file) {
+    function playlistDescription($file) {
         $data = file_get_contents($file);
         $playlist = json_decode($data, true);
         
         return $playlist["description"];
     }
-
 ?>
 
 <!DOCTYPE html>
@@ -52,15 +80,40 @@
     
     <body>
         <header>
-            <div class="titol">
+            <div class="page-title">
                 <h1>Gramola</h1>
             </div>
 
-            <div class="session">
-                <a href="login/login-form.php">
-                    <button>Iniciar Sessió</button>
-                </a>
+            <div class="burger">
+                <i class="fa-solid fa-bars"></i>
+
+                <nav>
+                    <ul>
+                        <?php 
+                            foreach ($files as $i => $file) { ?>
+                            <li><a href="index.php?playlist_id=<?= $i ?>"><?= playlistName($file);?></a>
+                                <ul>
+                                    <?php
+                                        for ($i = 0; $i < count($playlist["songs"]); $i++) {
+                                            $name = $playlist["songs"][$i]["title"];
+                                            $artist = $playlist["songs"][$i]["artist"];
+                                    ?>
+                                    <li>
+                                        <p id="canco<?php echo $i ?>" onclick="playlistSong(<?php echo $i ?>)"><?php echo $name; ?></p>
+                                        <p class="description"><?php echo $artist;?></p>
+                                    </li>
+                                    <?php } ?>
+                                </ul>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </nav>
             </div>
+
+            <h2>Playlist Més Reproduïda: 
+                <p><?php echo $nomMesReproduida; ?></p>
+            </h2>
+
         </header>
 
         <div class="principal">
@@ -72,12 +125,12 @@
                         foreach ($files as $i => $file) { ?>
                         <li>
                             <div class="playlist">
-                                <div class="nom-playlist">
+                                <div class="name-playlist">
                                     <a href="index.php?playlist_id=<?= $i ?>"><?= playlistName($file);?></a>
                                     <p class="description"><?= playlistDescription($file);?></p>
                                 </div>
 
-                                <div class="eliminar-playlist">
+                                <div class="delete-playlist">
                                     <a href="afegir/delete-playlist.php?playlist_id=<?= $i ?>">
                                         <i class="fa-solid fa-trash" title="Esborrar"></i>
                                     </a>
@@ -86,7 +139,7 @@
                         </li>
                     <?php } ?>
                 </ul>
-                <div class="afegir-playlist">
+                <div class="add-playlist">
                     <a href="afegir/add-playlist.php?playlist_id=<?= $playlistId ?>">
                         <button>Afegeix Nova Playlist</button>
                     </a>
@@ -97,8 +150,8 @@
             <!-- CONTAINER REPRODUCTOR -->
             <div class="container-reproductor">
 
-                <div class="img-canco">
-                    <img src="" class="activa" id="images">
+                <div class="img-song">
+                    <img src="" class="active" id="images">
                 </div>
 
                 <!-- Nom canço/artista -->
@@ -106,9 +159,9 @@
                 <h3 id="artista"></h3>
 
                 <!-- Barra del temps de la canço amb els minuts -->
-                <div class="barra" id="barra-progres">
+                <div class="bar" id="barra-progres">
                     <div class="progres" id="progres"></div>
-                    <div class="duracio">
+                    <div class="duration">
                         <span id="temps-actual">0:00</span>
                         <span id="total-temps">0:00</span>
                     </div>
@@ -119,7 +172,7 @@
                 <div class="controls">
                     <i class="fa-solid fa-backward" title="Anterior" id="anterior"></i>
                     <i class="fa-solid fa-shuffle" title="Aleatori" id="aleatori"></i>
-                    <i class="fa-solid fa-play boto-play" title="Reprodueix" id="play"></i>
+                    <i class="fa-solid fa-play play-button" title="Reprodueix" id="play"></i>
                     <i class="fa-solid fa-stop" title="Aturar" id="stop"></i>
                     <i class="fa-solid fa-forward" title="Següent" id="seguent"></i>
                 </div>
@@ -129,7 +182,7 @@
                         <i class="fa-solid fa-volume-high" id="icona-volum"></i>
                     </div>
 
-                    <div class="barra-volum">
+                    <div class="volume-bar">
                         <div class="progres-volum" id="progres-volum"></div>
                     </div>
                     
